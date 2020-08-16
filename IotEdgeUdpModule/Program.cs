1226c708e0becbe0801a5c3787ce53e57a75a243
Atul <beyondatul@hotmail.com>
2020-08-16 23:36:09 +0530

    commit 2

--- IotEdgeUdpModule/Program.cs
+++ IotEdgeUdpModule/Program.cs
@@ -70,7 +70,7 @@ namespace IotEdgeUdpModule
             Thread thdUDPServer = new Thread(new ThreadStart(UDPServer));
             thdUDPServer.Start();
 
-            Console.WriteLine("StartedNew");
+            Console.WriteLine("StartedNew2");
         }
 
         /// <summary>
